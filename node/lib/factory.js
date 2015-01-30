adf159ac542b9ef7f4da42a76fd82650c766ee68
Andrew Amis <atamiser@gmail.com>
2015-01-29 17:13:40 -0800

    Implemented factory for ping, pong, and smsg.

---
+++ node/lib/factory.js
@@ -0,0 +1,25 @@
+var version = require('./version')
+
+module.exports = (function() {
+  function header() {
+    return "COUL " + version + " "
+  }
+
+  this.ping = function() {
+    return header() + "PING\n\n"
+  }
+
+  this.pong = function() {
+    return header() + "PONG\n\n"
+  }
+
+  this.smsg = function(nick, server, channel, timestamp, message) {
+    if (message.slice(-1) != "\n") {
+      message = message + "\n"
+    }
+    return header() + "SMSG " + nick + "@" + server + " " + channel + " " + timestamp + "\n" + message + "\n"
+  }
+
+  return this;
+
+})()
---
+++ node/test/test_factory.js
@@ -0,0 +1,49 @@
+var factory = require('../lib/factory')
+var parser = require('../lib/wrapper')
+var assert = require('assert')
+var version = require('../lib/version')
+
+describe("factory", function() {
+  describe("#ping", function() {
+    it('should build a ping message', function() {
+      assert.equal("COUL " + version + " PING\n\n", factory.ping())
+    })
+    it('should build a parsable ping message', function(){
+      var obj = parser(factory.ping())
+      assert.deepEqual({"command":"PING"}, obj)
+    })
+  })
+
+  describe("#pong", function() {
+    it('should build a pong message', function() {
+      assert.equal("COUL " + version + " PONG\n\n", factory.pong())
+    })
+    it('should build a parsable pong message', function(){
+      var obj = parser(factory.pong())
+      assert.deepEqual({"command":"PONG"}, obj)
+    })
+  })
+
+  describe("#smsg", function() {
+    it('should build an smsg message', function() {
+      assert.equal("COUL " + version + " SMSG indigo@test.awesome.com bots 43287.453\nThis is a test.\nNice.\n\n",
+                  factory.smsg("indigo", "test.awesome.com", "bots", 43287.453, "This is a test.\nNice.\n"))
+      assert.equal("COUL " + version + " SMSG indigo@test.awesome.com bots 43287.453\nThis is a test.\nNice.\n\n",
+                  factory.smsg("indigo", "test.awesome.com", "bots", "43287.453", "This is a test.\nNice.\n"))
+    })
+    it('should deal with messages without newlines', function() {
+      assert.equal("COUL " + version + " SMSG indigo@test.awesome.com bots 43287.453\nThis is a test.\n\n",
+                  factory.smsg("indigo", "test.awesome.com", "bots", "43287.453", "This is a test."))
+    })
+    it('should build a parsable smsg message', function() {
+      var obj = parser(factory.smsg("indigo", "test.awesome.com", "bots", "12345.12345", "This is a test.\nNice.\n"))
+      assert.deepEqual({"command": "SMSG",
+                        "nick": "indigo",
+                        "server": "test.awesome.com",
+                        "channel": "bots",
+                        "timestamp": "12345.12345",
+                        "message": "This is a test.\nNice.\n"
+      }, obj)
+    })
+  })
+})
